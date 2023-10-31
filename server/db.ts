678b63bcfc5c4218880291d71d648fb5007c063a
josiah <yhafez3@gmail.com>
2023-10-30 23:29:20 -0700

    feat: updates

--- server/db.ts
+++ server/db.ts
@@ -1,25 +1,22 @@
-import dotenv from 'dotenv';
-import pg, { QueryConfig, QueryResult } from 'pg';
-const { Pool } = pg;
+import dotenv from 'dotenv'
+import pg, { QueryConfig, QueryResult } from 'pg'
+const { Pool } = pg
 
-dotenv.config();
+dotenv.config()
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: parseInt(process.env.DB_PORT || '5432'),
-});
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASS,
+	port: parseInt(process.env.DB_PORT || '5432'),
+})
 
 export const query = async (text: string, params?: QueryConfig['values']) => {
-    try {
-        console.log(0)
-        return await pool.query(text, params) as QueryResult<Record<string, unknown>>;
-    }
-    catch (err) {
-        console.error('Error executing query:', err);
-        throw err;
-    }
-
-};
+	try {
+		return (await pool.query(text, params)) as QueryResult<Record<string, unknown>>
+	} catch (err) {
+		console.error('Error executing query:', err)
+		throw err
+	}
+}
